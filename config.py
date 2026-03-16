1494e14335769cd06c92d24cca11767bcef9b768
skp81020-sys <skp81020@gmail.com>
2026-03-16 19:57:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = "7912605698:AAF9ci_dUvTKD9A_WZcjrGD2. EDIT HERE. "
-    API_ID = 271 EDIT HERE
-    API_HASH = "e166ccc508386322f47a EDIT HERE"
-    ADMIN = '752140,59194'.split(',')
+    BOT_TOKEN = "8667672710:AAE00K_U0nAJh6yvPCVTB9iwXxgiNJzYJps"
+    API_ID =32055524
+    API_HASH = "52777b469ffe310595012e55028d746d"
+    ADMIN = '6231590946,59194'.split(',')
     ADMIN_ID = [int(id) for id in ADMIN]
-    DB_URL = "mongodb+srv://n8360392852:JMXqsP0puz9qwgBe@cluster0.ce0ca.mongodb EDIT HERE  "
+    DB_URL = "mongodb+srv://satish0478a_db_user:l3AbN3LEtxAj7lcg@webxgpt.ltr0n72.mongodb.net/?retryWrites=true&w=majority&appName=bot"
     DB_NAME = "WARRIOR_EXTRACTOR"
     TXT_LOG = -10023125
     AUTH_LOG = -10023802
